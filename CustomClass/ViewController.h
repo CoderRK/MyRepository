dd778efeaa1f768741800180c3cb06dd9c66436f
renkui <iOS_renkui@163.com>
2015-04-15 15:19:06 +0800

    first commit

---
+++ CustomClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomClass
+//
+//  Created by 任奎 on 15/4/15.
+//  Copyright (c) 2015年 RK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomClass/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomClass
+//
+//  Created by 任奎 on 15/4/15.
+//  Copyright (c) 2015年 RK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
