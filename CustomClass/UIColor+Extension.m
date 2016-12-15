5a44f0a729770f307b22070abd4bccef4c187804
renkui <395990324@qq.com>
2016-12-15 16:56:31 +0800

    1.add launchImage 2.add color categray

---
+++ CustomClass/UIColor+Extension.m
@@ -0,0 +1,24 @@
+//
+//  UIColor+Extension.m
+//  RKBaseController
+//
+//  Created by 任奎 on 2016/12/12.
+//  Copyright © 2016年 RK. All rights reserved.
+//
+
+#import "UIColor+Extension.h"
+
+@implementation UIColor (Extension)
++ (UIColor *)colorWithHex:(int)hex
+{
+    return [UIColor colorWithHex:hex alpha:1.0f];
+}
+
++ (UIColor *)colorWithHex:(int)hex alpha:(CGFloat)alpha
+{
+    return [UIColor colorWithRed:((float)((hex & 0xFF0000) >> 16))/255.0
+                           green:((float)((hex & 0xFF00) >> 8))/255.0
+                            blue:((float)(hex & 0xFF))/255.0 alpha:alpha];
+}
+
+@end
